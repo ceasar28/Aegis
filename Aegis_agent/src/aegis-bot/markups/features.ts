2d0134489f9208121710b9172a79215dd7ea5d47
ceasar28 <emmanuelugochukwuekete@gmail.com>
2025-03-08 22:27:12 +0100

    markup update

--- Aegis_agent/src/aegis-bot/markups/features.ts
+++ Aegis_agent/src/aegis-bot/markups/features.ts
@@ -20,15 +20,15 @@ export const allFeaturesMarkup = async (user: UserDocument) => {
           }),
         },
       ],
-      [
-        {
-          text: `${user.enableAgenticAutoSwap ? `⇄ Agentic Auto bridge mode Enabled` : '⇌  Agentic Auto bridge mode Disabled'}`,
-          callback_data: JSON.stringify({
-            command: '/disableAgenticSwap',
-            language: 'english',
-          }),
-        },
-      ],
+      // [
+      //   {
+      //     text: `${user.enableAgenticAutoSwap ? `⇄ Agentic Auto bridge mode Enabled` : '⇌  Agentic Auto bridge mode Disabled'}`,
+      //     callback_data: JSON.stringify({
+      //       command: '/disableAgenticSwap',
+      //       language: 'english',
+      //     }),
+      //   },
+      // ],
       [
         {
           text: `${user.rebalanceEnabled ? `✅ Auto Rebalancing mode Enabled` : '🔄 Enable Auto Rebalancing agent mode'}`,
